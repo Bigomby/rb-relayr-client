0e4e5f9256d18997d58fdc37a68206d01a01d284
Diego Fernández Barrera <bigomby@gmail.com>
2016-01-23 01:08:22 +0100

    test(testing): Added base files for testing
    Added files for testing, but no test are working yet

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,7 @@
 var expect = require('chai').expect
-var rbRelayrClient = require('./index')
 
 describe('check testing', function () {
   it('should work', function () {
     expect(true).to.be.true
-  })
-})
-
-describe('testing tests', function () {
-  it('should salute', function () {
-    expect(rbRelayrClient.helloWorld).to.equal('Hello World')
   })
 })
